d911ad376a5ad944019232eb537addac6acc41a8
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-09-14 03:13:53 -0300

    Add an entry point to Evento Create inside aula info page.

---
+++ src/app/pages/aula/info/add-fab/add-fab.controller.js
@@ -0,0 +1,38 @@
+export default class AulaInfoAddFabController {
+  constructor($scope, $timeout, $location) {
+    'ngInject'
+    this.$scope    = $scope
+    this.$timeout  = $timeout
+    this.$location = $location
+
+    this.isAddFabOpen      = false
+    this.addTooltipVisible = false
+    this.timerEventName    = 'evento.addTooltip.timeout.destroy'
+    this.$scope.$watch(() => this.isAddFabOpen, this.addTooltipVisibility())
+  }
+
+  addTooltipVisibility () {
+    return (isOpen) => {
+      this.timer = this.$timeout(this.timerBehaviour(isOpen), 400)
+      this.$scope.$on(this.timerEventName, this.destroyTimeout());
+    }
+  }
+
+  timerBehaviour(isOpen) {
+    return () => {
+      this.addTooltipVisible = isOpen
+      this.$scope.$emit(this.timerEventName)
+    }
+  }
+
+  destroyTimeout() {
+    return (event) => { this.$timeout.cancel(this.timer) }
+  }
+
+  //
+  // Redirects
+  //
+  redirAddEvento() {
+    this.$location.path('/evento/create')
+  }
+}
